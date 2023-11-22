b4d40e9486432659b9cb02a889a707bd1f278a03
KhansaMahira <khansamahira011@gmail.com>
2023-11-22 10:10:00 +0700

    Link For Tugas 9

--- library/settings.py
+++ library/settings.py
@@ -67,6 +67,7 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+CSRF_TRUSTED_ORIGINS = ['https://khansa-mahira-tugas.pbp.cs.ui.ac.id/']
 
 ROOT_URLCONF = 'library.urls'
 
